1e8e4c5fd06d6abb0f8699616e8bd8de3f2685f5
Thomas Cannon <tcannon00@gmail.com>
2021-07-08 13:19:51 -0400

    Refactor `rendering.rb` to not use `alias_method_chain`
    * Rails 5.x has deprecated `alias_method_chain`, in favor of
    	`Module.prepend`

--- lib/prototype-rails/rendering.rb
+++ lib/prototype-rails/rendering.rb
@@ -1,13 +1,27 @@
 require 'action_view/helpers/rendering_helper'
 
 ActionView::Helpers::RenderingHelper.module_eval do
-  def render_with_update(options = {}, locals = {}, &block)
-    if options == :update
-      update_page(&block)
-    else
-      render_without_update(options, locals, &block)
+  if Rails::VERSION::MAJOR >= 5 && Rails::VERSION::MINOR >= 0
+    module RenderWithUpdate
+      def render(options = {}, locals = {}, &block)
+        if options == :update
+          update_page(&block)
+        else
+          super(options, locals, &block)
+        end
+      end
     end
+
+    prepend RenderWithUpdate
+  else
+    def render_with_update(options = {}, locals = {}, &block)
+      if options == :update
+        update_page(&block)
+      else
+        render_without_update(options, locals, &block)
+      end
+    end
+
+    alias_method_chain :render, :update
   end
-  
-  alias_method_chain :render, :update
 end
