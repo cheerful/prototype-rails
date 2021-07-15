490b637c989aee1367cb91182ab5385462ec7c60
Thomas Cannon <tcannon00@gmail.com>
2021-07-15 11:01:40 -0400

    Patch ActionView template handler for Rails 6+ support

--- lib/action_view/template/handlers/rjs.rb
+++ lib/action_view/template/handlers/rjs.rb
@@ -5,8 +5,16 @@ module ActionView
       class_attribute :default_format
       self.default_format = Mime[:js]
 
-      def call(template)
-        "update_page do |page|;#{template.source}\nend"
+      if Rails::VERSION::MAJOR == 6
+        def call(template, source)
+          "update_page do |page|;#{source}\nend"
+        end
+      end
+
+      if Rails::VERSION::MAJOR == 5
+        def call(template)
+          "update_page do |page|;#{template.source}\nend"
+        end
       end
     end
   end
