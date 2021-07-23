c60d8a7ed3528ddc31d40df140bd107e3905e90f
Thomas Cannon <tcannon00@gmail.com>
2021-07-23 11:45:38 -0400

    Fix `prototype-rails` initializer to work nicely with `zeitwerk`
    * Initializers should only be called after the framework has been
    	initialized

--- lib/prototype-rails.rb
+++ lib/prototype-rails.rb
@@ -3,13 +3,15 @@ require 'active_support'
 
 module PrototypeRails
   class Engine < Rails::Engine
-    initializer 'prototype-rails.initialize' do
-      ActiveSupport.on_load(:action_controller) do
-        require 'prototype-rails/on_load_action_controller'
-      end
+    config.after_initialize do
+      initializer 'prototype-rails.initialize' do
+        ActiveSupport.on_load(:action_controller) do
+          require 'prototype-rails/on_load_action_controller'
+        end
 
-      ActiveSupport.on_load(:action_view) do
-        require 'prototype-rails/on_load_action_view'
+        ActiveSupport.on_load(:action_view) do
+          require 'prototype-rails/on_load_action_view'
+        end
       end
     end
   end
